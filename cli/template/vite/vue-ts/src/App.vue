31c69b136bd674a0d81de2c2d55f557b2489a86f
gotoo123 <1219727892@qq.com>
2024-04-14 19:57:02 +0800

    feat: add vite/vue-ts template

---
+++ cli/template/vite/vue-ts/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app">{{ welcome }}</div>
+  <child />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import Child from './Child.vue';
+
+const welcome = ref('hello vite & vue-ts!');
+let a: number = 3;
+a = [3];
+console.log(a)
+
+</script>
+
+<style scoped>
+@import './App.less';
+
+.app {
+  margin: 30px;
+  font-size: 30px;
+  font-weight: 600;
+}
+</style>
